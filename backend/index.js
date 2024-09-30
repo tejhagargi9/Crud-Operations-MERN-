3ba421c6bf1108fa6187cffaf35f2c63d2d10fab
Tej MH <tejhagargigit@gmail.com>
2024-09-30 22:38:49 +0530

    docker file creation

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/crud");
+mongoose.connect("mongodb://mongo:27017/crud");
 
 app.get("/", (req, res) => {
   UserModel.find({})
